ceb3a31dc31f5ddb6bd592dc4e30fba519c8113a
usha rana <usharana@Ushas-MacBook-Pro.local>
2022-04-06 10:31:07 -0400

    Added pom.xml and src

---
+++ src/main/java/com/kuebiko/stockmonitor/StockMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.kuebiko.stockmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuebiko/stockmonitor/StockMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuebiko.stockmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
